c0fe8a0701194e0982f278a92b81361699120dd3
Martin Carnogursky <intense.feel@gmail.com>
2020-09-05 15:36:25 +0200

    add coverage reporting

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,13 @@ set -e
 
 if [ $1 == "run_tests" ]; then
     cd /analyzer
-    exec pytest --cov=aura tests/
+    exec pytest --cov=aura --cov-report xml --cov-report term tests/
+
+    if [[ -d "/shared" && -f coverage.xml ]]; then
+      echo "Copying coverage report to the /shared folder"
+      cp coverage.xml /shared/
+    fi
+
 fi;
 
 exec aura "$@"
